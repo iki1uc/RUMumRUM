5b7c8a1ea08e9daa3b0af7588d6837278deb69a3
iki1uc <entertrainer@web.de>
2026-08-11 14:00:08 +0200

    Create VECTOR.js

---
+++ VECTOR.js
@@ -0,0 +1,8 @@
+export const NC6 = {
+  1: { id: "◆", name: "Ursprung" },
+  2: { id: "◉", name: "Ursache" },
+  3: { id: "◎", name: "Wirkung" },
+  4: { id: "▣", name: "Continuum" },
+  5: { id: "⚚", name: "Orbit / rumumrum" },
+  6: { id: "Σ", name: "Summation / ID.ICOME" }
+};
